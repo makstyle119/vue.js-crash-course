54d1dc513652f5a8391a45798be217e7677e730d
Mohammad Moiz Ali <moizprince786@gmail.com>
2025-02-13 23:05:03 +0500

    Options API data() & Interpolation

--- create-vue/src/App.vue
+++ create-vue/src/App.vue
@@ -1,13 +1,15 @@
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
 <template>
   <h1>
-    Vue Job
+    {{ msg }}
   </h1>
 </template>
-
-<style scoped> 
-  /* Add scoped to make it local */
-  h1 {
-    font-size: 2em;
-    color: #42b983;
-  }
-</style>
